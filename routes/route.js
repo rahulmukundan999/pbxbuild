6111fff094b980463cf14b998114170e19b44c4c
rahulmukundan <rahulmukundan999@gmail.com>
2018-12-23 11:11:22 +0530

    fwefj

--- routes/route.js
+++ routes/route.js
@@ -427,9 +427,12 @@ router.get('/api/trunks',verify.common,(req,res,next)=>{
     }
 }
         xml1.end({ pretty: true});
-        fs.writeFile("/usr/local/freeswitch/conf/sip_profiles/external/gateway.xml",xml1,function(err){
+        // fs.writeFile("/usr/local/freeswitch/conf/sip_profiles/external/gateway.xml",xml1,function(err){
 
-         });
+        //  });
+        fs.writeFile("./trunk/gateway.xml",xml1,function(err){
+
+        });
          res.json(trunks);
         
         });
